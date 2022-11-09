3f215e5782c8b2b9796b560b9187cf9f7e0af6db
onesimu <872634214@qq.com>
2022-11-09 15:42:56 +0800

    upt

--- mock/ut.js
+++ mock/ut.js
@@ -55,5 +55,5 @@ const fn2 = ts => {
 // fs('/src/views/detail.vue', fn1)
 
 const wd = fs('./up.txt')
-const d = wd.t().t('\n').n().t(1).t(i => i.split(/\s+/)).t(i => `${i[0]}@${i[3]}`).join(' ')
+const d = wd.t().t('\n').n().t(1).t(i => i.split(/\s+/)).t(i => `${i[0]}@${i[2]}`).join(' ')
 o('pnpm update ' + d)

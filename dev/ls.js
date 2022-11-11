f7505554f178f9396aced933c3dc50b875649c1d
onesimu <872634214@qq.com>
2022-11-11 10:15:24 +0800

    clean

--- dev/ls.js
+++ dev/ls.js
@@ -23,7 +23,6 @@ module.exports = (options = {}) => {
                   .e(/^import .+/gm, '')
                   .e(/^export default/gm, '``$&``')
                 const output = livescript.compile(cod, options)
-                // console.log(output.code)
                 const data = output.code.match(/^var .+/gm).t(',').e('var', '').e(';', '')
                 const code3 = `
                   ${imp}
@@ -38,13 +37,8 @@ module.exports = (options = {}) => {
                     code: code3,
                     map: output.map.toString()
                 }
-
-              // var style = /\<style.*?lang=qcs.*?\>([\s\S]+?)\<\/style\>/
-              // var lse = /\<script.*?lang=ls.*?\>([\s\S]+?)\<\/script\>/
-              // data.e(lse, '').e(style, function(i, m){ return '<style>' + u.qcs(m) + '</style>' })
             }
             if (/\.scss/.test(id)) {
-                // console.log(id, code)
 
                 var oldCssText = code
                 var newCssText = oldCssText
@@ -53,27 +47,11 @@ module.exports = (options = {}) => {
                   newCssText = qcs(c)
                   if (process.env.isMiniprogram) newCssText = newCssText.replace(/px/g, 'rpx')
                 }
-                // console.log(newCssText)
-            // E:/dev/vue2-admin-vite/src/views/dashboard/index.vue?vue&type=style&index=0&scoped=48f440f2&lang.qcs
-
-                const code3 = `
-                import { createHotContext as __vite__createHotContext } from "/@vite/client";
-                import.meta.hot = __vite__createHotContext("${id.slice(id.indexOf('/src'))}");
-                import { updateStyle as __vite__updateStyle, removeStyle as __vite__removeStyle } from "/@vite/client"
-                const __vite__id = "${id}"
-                const __vite__css = "${newCssText}"
-                __vite__updateStyle(__vite__id, __vite__css)
-                import.meta.hot.accept()
-                export default __vite__css
-                import.meta.hot.prune(() => __vite__removeStyle(__vite__id))
-                `
-                // console.log(code3)
 
                 return {
                     code: newCssText,
                     map: {}
                 }
-
 
             }
         }
